d356fa1c4ccd95e998e423cfcbfa8fceea3d7eca
farhan2659 <52378426+farhan2659@users.noreply.github.com>
2019-08-12 12:43:44 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,11 @@
+pkg install figlet -y
+ 
+pkg install toilet -y
+pkg install python -y
+pkg install python2 -y
+pkg install curl
+pkg install perl
+pip2 install --upgrade pip
+pkg install ruby
+gem install lolcat
+ 
